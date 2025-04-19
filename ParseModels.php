cc1406e789482842c841d7cd615755b28e855eba
Freddie Sackur <freddie.git@dustyfox.uk>
2025-04-19 17:31:31 +0100

    parse BaseModel

---
+++ ParseModels.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace OPNsense\OpenApi\Parsing;
+
+use Exception;
+use ReflectionClass;
+use ReflectionMethod;
+use ReflectionParameter;
+use ReflectionException;
+use OPNsense\Base\BaseModel;
+
+require_once(dirname(__FILE__) . '/ParserBase.php');
+
+
+class Model extends ParsedBase {
+    private $instance;
+
+    public function __construct(ReflectionClass $rclass, Model | null $parent)
+    {
+        parent::__construct($rclass, $parent);
+        $name = $this->name;
+
+        if ($this->is_abstract) {
+            return;
+        }
+        $this->instance = $rclass->newInstance();
+    }
+
+    public function getData() {
+        $this->instance->Default();
+        return $this->instance->getNodes();
+    }
+
+    public function validate(BaseField $nodes) {
+        $this->instance->performValidation(true);
+    }
+}
+
+
+class ModelRegistry extends Registry {}
+
+
+$base_path = $config->__get("application")->modelsDir;
+$parser = new Parser(
+    new ReflectionClass(Model::class),
+    new ReflectionClass(BaseModel::class),
+    new ReflectionClass(ModelRegistry::class),
+    $path_regex = "/models\/\w+\/\w+\/\w+\.php/",
+);
+
+if ($output_file) {
+    $parser->export($base_path, $output_file, true);
+}
+
+$models = $parser->get_all($base_path);
+
+
+foreach ($models as $model) {
+    if ($model->is_abstract) {continue;}
+    $data = $model->getData();
+
+    var_dump($data);
+}
+
+?>
