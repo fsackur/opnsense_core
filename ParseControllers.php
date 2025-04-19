91bd830c51f6544fee31832dfa9636b4eea23f50
Freddie Sackur <freddie.git@dustyfox.uk>
2025-04-19 11:13:18 +0100

    factor out ParserBase

---
+++ ParseControllers.php
@@ -0,0 +1,283 @@
+<?php
+/**
+ * Parse controller classes using reflection, to minimise regex parsing.
+ *
+ * I do the bare minimum in PHP because a) I don't know PHP and b) type system
+ * is not great.
+ *
+ * Called from `parse_endpoints.py`.
+ *
+ * USAGE:
+ *      php ParseControllers.php [ARGS]
+ *
+ * ARGS:
+ *      -o, --output-file      path to write a JSON file
+ */
+
+namespace OPNsense\OpenApi\Parsing;
+
+use Exception;
+use ReflectionClass;
+use ReflectionMethod;
+use ReflectionParameter;
+use ReflectionException;
+use OPNsense\Mvc\Controller as ControllerRootClass;
+
+require_once(dirname(__FILE__) . '/ParserBase.php');
+
+
+class Parameter {
+    public $name;
+    public $has_default;
+    public $default;
+
+    public function __construct(ReflectionParameter $rparam) {
+        $this->name = $rparam->name;
+        if ($rparam->isDefaultValueAvailable()) {
+            $this->has_default = true;
+            $this->default = $rparam->getDefaultValue();
+        } else {
+            $this->has_default = false;
+        }
+    }
+}
+
+
+class Method {
+    protected static $BASE_METHOD_HTTP_METHODS = [
+        "request->isPost" => "POST",
+        "request->hasPost" => "POST",
+        "request->getPost" => "POST",
+        "setAction" => "POST",
+        "delBase" => "POST",
+        "addBase" => "POST",
+        "setBase" => "POST",
+        "toggleBase" => "POST",
+        "searchBase" => "POST",  // GET works for backward-compatibility, but isn't worth publishing
+        "searchRecordsetBase" => "POST",
+    ];
+
+    protected static $BASE_METHOD_REQUIRE_BODY = [
+        "request->getPost",
+        "setAction",
+        "addBase",
+        "setBase",
+    ];
+
+    public $name;
+    public $method;  // HTTP method!
+    public $parameters = [];
+    public $doc;
+    public $requires_body;
+    public $model_path_map;
+    public $request_model;
+    public $response_model;
+
+    public function __construct(ReflectionMethod $rmethod, string $src)
+    {
+        $name = preg_replace("/Action\$/", "", $rmethod->name);
+        $request_model = null;
+        $response_model = null;
+
+        // Presence in source code of, e.g., "this->request->getPost(" implies POST.
+        // See comment in Controller ctor.
+        $matches = null;
+        $after_deref = "(?<=\\\$this->)";   // lookbehind for "$this->"
+        $call = "([^\(]+)";                 // match until opening bracket
+        $round_bracket = "(?:\(\\s*)";      // don't capture
+        $first_argument = "([^,^\)^\s]*)";  // not comma, closing bracket, or space
+        $pattern = "/" . $after_deref . $call . $round_bracket . $first_argument . "/";
+        preg_match_all($pattern, $src, $matches);
+
+        $http_method = null;
+        $requires_body = False;
+        foreach ($matches[1] as $idx => $call) {
+            if (array_key_exists($call, self::$BASE_METHOD_HTTP_METHODS)) {
+                $http_method = self::$BASE_METHOD_HTTP_METHODS[$call];
+                $requires_body = $requires_body || in_array($call, self::$BASE_METHOD_REQUIRE_BODY);
+            }
+        }
+        if (!$http_method) {$http_method = "GET";}
+
+        $matches = null;
+        $pattern = "/\\\$this->((search|searchRecordset|get|add|del|set|toggle)Base|request->getPost)\(([^\)]*)\)/";
+        preg_match_all($pattern, $src, $matches);
+
+        $model_path_map = null;
+        if ($matches[0]) {
+            // Multiple calls to delBase in IPsec\Api\ConnectionsController and
+            // Unbound\Api\SettingsController. In both cases, the earlier calls are
+            // "cascade deletes", and it seems probable that the last call is always
+            // going to be the "main" model change. So we'll take the last match
+            // for each capture group.
+            $call = array_slice($matches[0], -1)[0];
+            $base_method = array_slice($matches[1], -1)[0];
+            $args = preg_split("/,\s*/", trim(array_slice($matches[3], -1)[0]));
+
+            if (in_array($base_method, ["searchBase", "searchRecordsetBase"])) {
+                $request_model = "search.request";
+                $response_model = "search.response";
+            } elseif (in_array($base_method, ["addBase", "setBase", "getBase"])) {
+                $model_path_map = $args[0] . ":" . $args[1];
+            } elseif (in_array($base_method, ["toggleBase", "delBase"])) {
+                $response_model = "result";
+            } elseif ($base_method === "request->getPost") {
+                $model_path_map = $args[0] . ":";
+            } else {
+                $model_path_map = ":" . $args[0];
+            }
+
+            if ($model_path_map) {
+                $model_path_map = preg_replace("/\"|'/", "", $model_path_map);
+            }
+        }
+
+        $params = [];
+        $rparams = $rmethod->getParameters();
+        foreach ($rparams as $rparam) {
+            $params[] = new Parameter($rparam);
+        }
+
+        $doc = $rmethod->getDocComment();
+        if (preg_match("/@inheritdoc/", $doc)) {
+            $rclass = $rmethod->getDeclaringClass();
+            $rparent = $rclass->getParentClass();
+            $parent = ControllerRegistry::get($rparent->getName());
+            $parent_method = array_filter($parent->methods, function ($method) use ($name) {
+                return $method->name === $name;
+            })[0];
+            $doc = $parent_method->doc;
+        }
+
+        $this->name = $name;
+        $this->method = $http_method;
+        $this->parameters = $params;
+        $this->doc = $doc;
+        $this->requires_body = $requires_body;
+        $this->model_path_map = $model_path_map;
+        $this->request_model = $request_model;
+        $this->response_model = $response_model;
+    }
+}
+
+
+class Controller extends ParsedBase {
+    public $methods = [];
+    public $model;
+    public $model_name;
+
+    public function __construct(ReflectionClass $rclass, Controller | null $parent)
+    {
+        parent::__construct($rclass, $parent);
+        $name = $this->name;
+
+        $model = null;
+        try {
+            $prop = $rclass->getProperty("internalModelClass");
+            if ($prop) {
+                $model = $prop->getDefaultValue();
+            }
+        } catch (ReflectionException $e) {
+            if ($parent) {
+                $model = $parent->model;
+            }
+        }
+
+        $model_name = null;
+        try {
+            $prop = $rclass->getProperty("internalModelName");
+            if ($prop) {
+                $model_name = $prop->getDefaultValue();
+            }
+        } catch (ReflectionException $e) {
+            if ($parent) {
+                $model_name = $parent->model_name;
+            }
+        }
+
+        if ($model && $model[0] == "\\") {
+            $model = substr($model, 1);
+        }
+
+        $doc = $rclass->getDocComment();
+        if (preg_match("/@inheritdoc/", $doc)) {
+            // there's only one class with no parent, and it doesn't use @inheritdoc
+            $doc = $parent->doc;
+        }
+
+        $this->model = $model;
+        $this->model_name = $model_name;
+
+        /**
+         * A route does not define an HTTP method - all routes accept all methods.
+         *
+         * `collect_api_endpoints.py` in the `docs` repo looks for method calls that imply
+         * POST. This makes me nervous. There is no way to defend against, e.g.:
+         *     if (request->isPost()) {throw WrongMethod("ha ha sucker")}
+         *
+         * In my view, when Ad talks about complexity and maintenance, this is the danger.
+         *
+         * I presume that unusual usage would be picked up in plugin code review, though. So I
+         * assume: it's been working for years, it's good enough.
+         *
+         * I considered nikic/PHP-Parser, but I don't think it's worth adding a dependency.
+         */
+        $filename = $rclass->getFileName();
+        $fd = fopen($filename, "r") or die("Failed to read '" . $filename . "'");
+        $src = fread($fd, 1000000);
+        fclose($fd);
+        $src_lines = explode("\n", $src);
+
+        $methods = [];
+        if ($parent) {
+            foreach ($parent->methods as $m) {
+                $methods[$m->name] = $m;
+            }
+        }
+        foreach ($rclass->getMethods(ReflectionMethod::IS_PUBLIC) as $rmethod) {
+            // skip inherited (we already added them)
+            if ($rmethod->getDeclaringClass()->name != $name) {continue;}
+
+            $method_name = $rmethod->name;
+            if (!str_ends_with($method_name, "Action")) {continue;}  // algorithm in Dispatcher.php
+
+            // get the body of the method as raw source code - see earlier comment
+            $start = $rmethod->getStartLine();
+            $length = $rmethod->getEndLine() - $start;
+            $method_src = implode("\n", array_slice($src_lines, $start, $length));
+
+            $method = new Method($rmethod, $method_src);
+            $methods[$method->name] = $method;
+        }
+
+        if ($name == "OPNsense\\Base\\ApiMutableModelControllerBase") {
+            $methods["set"]->response_model = "result";
+        } elseif ($name == "OPNsense\\Base\\ApiMutableServiceControllerBase") {
+            $methods["start"]->response_model = "response";
+            $methods["stop"]->response_model = "response";
+            $methods["restart"]->response_model = "response";
+            $methods["reconfigure"]->response_model = "status";
+            $methods["status"]->response_model = "status";
+        } elseif ($name == "OPNsense\\Monit\\Api\\SettingsController") {
+            $methods["getGeneral"]->model_path_map = "monit:general";
+        }
+
+        $this->methods = array_values($methods);
+    }
+}
+
+
+class ControllerRegistry extends Registry {}
+
+
+$base_path = $config->__get("application")->controllersDir;
+$parser = new Parser(
+    new ReflectionClass(Controller::class),
+    new ReflectionClass(ControllerRootClass::class),
+    new ReflectionClass(ControllerRegistry::class),
+    $path_regex = "/\/Api\/\w+Controller/",
+);
+
+echo $parser->export($base_path, $output_file, true);
+
+?>
---
+++ ParserBase.php
@@ -0,0 +1,232 @@
+<?php
+/**
+ * Parse controller classes using reflection, to minimise regex parsing.
+ *
+ * I do the bare minimum in PHP because a) I don't know PHP and b) type system
+ * is not great.
+ *
+ * Called from `parse_endpoints.py`.
+ *
+ * USAGE:
+ *      php ParseControllers.php [ARGS]
+ *
+ * ARGS:
+ *      -o, --output-file      path to write a JSON file
+ */
+
+namespace OPNsense\OpenApi\Parsing;
+
+use InvalidArgumentException;
+use ReflectionClass;
+use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
+
+$DEFAULT_SOURCE_DIR = "/usr/local/opnsense/mvc/app";
+
+
+abstract class ParsedBase {
+    public $name;
+    public $parent;
+    public $is_abstract;
+    public $doc;
+
+    public function __construct(ReflectionClass $rclass, ParsedBase | null $parent)
+    {
+        $parent_name = null;
+        $doc = $rclass->getDocComment();
+
+        if ($parent)
+        {
+            $parent_name = $parent->name;
+
+            if (preg_match("/@inheritdoc/", $doc)) {
+                $doc = $parent->doc;
+            }
+        }
+
+        $this->name = $rclass->getName();
+        $this->parent = $parent_name;
+        $this->is_abstract = $rclass->isAbstract();
+        $this->doc = $doc;
+    }
+}
+
+
+/**
+ * INVARIANT: parent is always registered before child
+ */
+abstract class Registry {
+    private static ReflectionClass $generic_class;
+    private static ReflectionClass $root_class;
+    private static $registry = array();
+
+    public static function init(ReflectionClass $generic_class, ReflectionClass $root_class) {
+        self::$generic_class = $generic_class;
+        self::$root_class = $root_class;
+    }
+
+    public static function register(ReflectionClass $rclass) {
+        $name = $rclass->getName();
+        if (array_key_exists($name, self::$registry)) {
+            return;
+        }
+
+        $rparent = $rclass->getParentClass();
+        if ($rparent) {
+            self::register($rparent);
+            $parent = self::get($rparent->name);
+        } else {
+            $parent = null;
+        }
+
+        if (!$parent && $rclass != self::$root_class) {
+            return;
+        }
+
+        $obj = self::$generic_class->newInstance($rclass, $parent);
+        self::$registry[$name] = $obj;
+    }
+
+    public static function get(string $name) {
+        if (array_key_exists($name, self::$registry)) {
+            return self::$registry[$name];
+        }
+    }
+
+    public static function dump() {
+        return array_values(self::$registry);
+    }
+}
+
+
+class Parser {
+    private ReflectionClass $generic_class;
+    private ReflectionClass $root_class;
+    private ReflectionClass $registry;
+    private string $path_regex;
+
+    public function __construct(
+        ReflectionClass $generic_class,
+        ReflectionClass $root_class,
+        ReflectionClass $registry,
+        string $path_regex,
+    ) {
+        $this->generic_class = $generic_class;
+        $this->root_class = $root_class;
+        $this->registry = $registry;
+        $this->path_regex = $path_regex;
+        $registry->getMethod("init")->invoke(null, $generic_class, $root_class);
+    }
+
+    public function find_classes(string $base_path) {
+        $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($base_path));
+        $class_names = array();
+
+        foreach ($rii as $file) {
+            if (
+                $file->isDir() ||
+                !str_ends_with($file, ".php") ||
+                !preg_match($this->path_regex, $file)
+            ) {continue;}
+
+            $rel_path = preg_replace("/.*(?=OPNsense)/", "", $file);
+            $class_name = preg_replace("/\.php$/", "", $rel_path);
+            $class_name = preg_replace("/\//", "\\", $class_name);
+            $class_names[] = $class_name;
+        }
+
+        return $class_names;
+    }
+
+    public function register_classes(array $class_names)
+    {
+        $register = $this->registry->getMethod("register");
+        foreach ($class_names as $c) {
+            $rclass = new ReflectionClass($c);
+            $register->invoke(null, $rclass);
+        }
+    }
+
+    public function get_all($base_path)
+    {
+        $class_names = $this->find_classes($base_path);
+        $this->register_classes($class_names);
+
+        $dump = $this->registry->getMethod("dump");
+        return $dump->invoke(null);
+    }
+
+    public function export($base_path, $output_file = null, $pretty = false)
+    {
+        $json_flags = JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE;
+        if ($pretty) {
+            $json_flags = $json_flags | JSON_PRETTY_PRINT;
+        }
+
+        $output = $this->get_all($base_path);
+        $json = json_encode($output, $json_flags) . "\n";
+
+        if ($output_file) {
+            $fd = fopen($output_file, "w") or die("Failed to touch '" . $output_file . "'");
+            fwrite($fd, $json);
+            fclose($fd);
+        } else {
+            return $json;
+        }
+    }
+}
+
+
+//region argparse
+$opts = getopt("s:o:", ["source-folder:", "output-file:"]);
+if (array_key_exists("o", $opts)) {
+    $output_file = $opts["o"];
+} elseif (array_key_exists("output-file", $opts)) {
+    $output_file = $opts["output-file"];
+} else {
+    $output_file = null;
+}
+
+if (array_key_exists("s", $opts)) {
+    $source_folder = $opts["s"];
+} elseif (array_key_exists("source-folder", $opts)) {
+    $source_folder = $opts["source-folder"];
+} else {
+    $source_folder = null;
+}
+
+if (!$source_folder) {
+    $source_folder = $DEFAULT_SOURCE_DIR;
+    $app_dir = realpath($source_folder);
+} else {
+    $app_base = $source_folder;
+    while ($app_base != "/") {
+        $app_dir = realpath($app_base . "/mvc/app");
+        if ($app_dir) {break;}
+        $app_base = dirname($app_base);
+    }
+}
+if (!$app_dir) {
+    throw new InvalidArgumentException("Could not find 'mvc/app' folder in any parent of " . $source_folder);
+}
+
+$contrib_base = $app_dir;
+while ($contrib_base != "/") {
+    $contrib_dir = realpath($contrib_base . "/contrib");
+    if ($contrib_dir) {break;}
+    $contrib_base = dirname($contrib_base);
+}
+if (!$contrib_dir) {
+    throw new InvalidArgumentException("Could not find 'contrib' folder in any parent of " . $app_dir);
+}
+//endregion argparse
+
+
+$config = require $app_dir . "/config/config.php";
+$config->update('globals.config_path', __DIR__ . "/");
+$config->update('application.contribDir', $contrib_base . "/contrib");
+
+set_include_path($contrib_dir);
+require $app_dir . "/config/loader.php";
+
+?>
